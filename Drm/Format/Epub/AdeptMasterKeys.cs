8eab6e059875315ba73ebb7fe8aa5ea381aa96f2
NoReply Amazon <no-reply@amazon.com>
2024-11-13 19:18:25 +0000

    Revert "[Commit] Iteration 4 #errors = 3 <= 2429."
    This reverts commit be2af6c4811243dda40e65b349e00e2dab3fa587.

--- Drm/Format/Epub/AdeptMasterKeys.cs
+++ Drm/Format/Epub/AdeptMasterKeys.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Management;
 using System.Runtime.CompilerServices;
@@ -141,11 +141,6 @@ public static partial class AdeptMasterKeys
 
 	[GeneratedRegex(@"\A\b[0-9a-fA-F]+\b\Z")]
 	private static partial Regex HexStringPattern();
-
-	private static partial Regex HexStringPattern()
-	{
-		return HexStringPatternRegex();
-	}
 	private const string DeviceKey = @"Software\Adobe\Adept\Device";
 	private const string ActivationKey = @"Software\Adobe\Adept\Activation";
 }
