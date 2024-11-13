35a6e3b3a46e250147d5e822e06bce52aeb84298
NoReply Amazon <no-reply@amazon.com>
2024-11-13 19:17:27 +0000

    [Commit] Iteration 2 #errors = 1 <= 2429.
    Addressing `BuildError `[/tmp/tempfspartition/QnetTransformArtifacts/MidTransformCode/sourceCode/Drm/Format/Epub/AdeptMasterKeys.cs][CS8795][143,31] Partial method 'AdeptMasterKeys.HexStringPattern()' must have an implementation part because it has accessibility modifiers.``.

--- Drm/Format/Epub/AdeptMasterKeys.cs
+++ Drm/Format/Epub/AdeptMasterKeys.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Management;
 using System.Runtime.CompilerServices;
@@ -140,7 +140,10 @@ public static partial class AdeptMasterKeys
 	}
 
 	[GeneratedRegex(@"\A\b[0-9a-fA-F]+\b\Z")]
-	private static partial Regex HexStringPattern();
+	private static partial Regex HexStringPattern()
+	{
+		return new Regex(@"\A\b[0-9a-fA-F]+\b\Z", RegexOptions.Compiled);
+	}
 	private const string DeviceKey = @"Software\Adobe\Adept\Device";
 	private const string ActivationKey = @"Software\Adobe\Adept\Activation";
 }
